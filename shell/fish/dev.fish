564fd301ff9c1810f0c709bf6616db1588850dbd
vitax <caglar.oezel@googlemail.com>
2021-09-18 00:22:53 +0200

    cleaning up

--- shell/fish/dev.fish
+++
@@ -1,9 +0,0 @@
-## Create aliases for commonly used remote desktop session
-
-# Work
-# ----
-alias rdp_work_pc 'xfreerdp /d:test.local /u:adminCaglar /v:192.168.1.100 /dynamic-resolution'
-alias rdp_work_domain 'xfreerdp /d:test.local /u:adminCaglar /v:192.168.1.241 /dynamic-resolution'
-
-#
-alias vpn_work 'sudo openvpn $HOME/vpn/adminCaglar@aikuxlab.sytes.net.ovpn'
